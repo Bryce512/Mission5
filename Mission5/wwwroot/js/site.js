62ee2bc775000338f1290c60cf2e39b11f4d4a52
Bryce512 <bryce.whitworth@gmail.com>
2025-02-05 11:33:01 -0700

    initial commit

---
+++ Mission5/wwwroot/js/site.js
@@ -0,0 +1,15 @@
+﻿$(document).ready(function () {
+    $("#calculateBtn").click(function () {
+        let hours = parseFloat($("#hours").val()); // get num hours from form
+        let rate = 40; // Hourly rate
+
+        // check input is a positive number
+        if (isNaN(hours) || hours <= 0) {
+            alert("Please enter a valid number of hours (greater than 0).");
+            $("#total").val(""); // Clear the old output if exists
+        } else {
+            let total = hours * rate;
+            $("#total").val(total.toFixed(2)); // Display total 
+        }
+    });
+});
